8df8c1a5657a9a6b37010b283902a4b314aa5468
ismaelis123 <romeroismael965@gmail.com>
2025-04-28 14:17:49 -0600

    nuevas funcions

--- src/controllers/ventas.controller.js
+++ src/controllers/ventas.controller.js
@@ -1,5 +1,35 @@
 import { pool } from '../db.js';
 
+// Obtener todas las ventas
+// Obtener todas las ventas
+export const obtenerVentas = async (req, res) => {
+  try {
+    const [result] = await pool.query(`
+      SELECT 
+        v.id_venta,
+        v.fecha_venta,
+        CONCAT(e.primer_nombre, ' ', e.primer_apellido) AS nombre_empleado,
+        CONCAT(c.primer_nombre, ' ', c.primer_apellido) AS nombre_cliente,
+        v.total_venta
+      FROM Ventas v
+      INNER JOIN Clientes c ON v.id_cliente = c.id_cliente
+      INNER JOIN Empleados e ON v.id_empleado = e.id_empleado
+    `);
+    
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ha ocurrido un error al leer los datos de las ventas.',
+      error: error
+    });
+  }
+};
+
+
+
+
+
+
 // Obtener todas las ventas con sus detalles, mostrando nombres, IDs y subtotal
 export const obtenerVentasConDetalles = async (req, res) => {
   try {
